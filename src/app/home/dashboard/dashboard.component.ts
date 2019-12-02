278f93ace2b9928e45a6c26b5df45677c7b0538c
Krishna Arkala <karkala@ets.org>
2019-12-02 18:09:25 -0500

    Added Demo Service

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  dataArr:any[] = [];
+  dataJson:any;
 
   constructor(private _service: DemoService) {
    }
@@ -16,7 +16,7 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     this._service.getAllBreeds().subscribe((data: any) => {
       console.log(data);
-      this.dataArr = data.message.terrier;
+      this.dataJson = data;
     });
   }
 
