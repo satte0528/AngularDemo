d65831102bef12055b253db8153d341c07b4f964
Krishna Arkala <karkala@ets.org>
2019-12-08 12:46:33 -0500

    Added Demo Service

--- src/app/core/services/demo.service.ts
+++ src/app/core/services/demo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class DemoService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getAllBreeds() {
+  getAllBreeds() : Observable<any> {
     console.log('Get All Breeds');
     return this.httpClient.get('https://dog.ceo/api/breeds/list/all');
   }
